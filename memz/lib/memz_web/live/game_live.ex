8b55844e440ed27da8e95ec2d3d43fd4eb528f69
okothkongo <okothkongo@gmail.com>
2021-01-28 21:19:23 +0300

    create eraser component

--- memz/lib/memz_web/live/game_live.ex
+++ memz/lib/memz_web/live/game_live.ex
@@ -3,29 +3,13 @@ defmodule MemzWeb.GameLive do
   alias Memz.Game
 
   def mount(_params, _session, socket) do
-    steps_total = 4
-    paragraph = "Did your try turning it off and on again?"
-    {:ok, assign(socket, game: Game.new(steps_total, paragraph))}
+    {:ok, socket}
   end
 
   def render(assigns) do
     ~L"""
-    <h1><%= @game.paragraph %></h1>
-    <h2>
-      <%= @game.steps_total %>
-    </h2>
-    <button phx-click="erase">
-      Erase
-    </button>
+    <h1> Welcome</h1>
+    <%= live_component @socket, MemzWeb.EraserComponent, id: "game" %>
     """
-  end
-
-  def handle_event("erase", _, socket) do
-    {:noreply, erase(socket)}
-  end
-
-  defp erase(socket) do
-    socket
-    |> assign(game: Game.erase(socket.assigns.game))
   end
 end
